8fdc63dcae9e11077e1d0b8bca7a44ec75ad3c8f
Tom Kumschier <dev.angelicornis@icloud.com>
2015-02-23 21:48:22 +0100

    Funktionen zur Berechnung wurde hinzugefügt Plus, Mal, Geteilt voll funktionstüchtig

--- erweiterter Taschenrechner/ViewController.swift
+++ erweiterter Taschenrechner/ViewController.swift
@@ -45,8 +45,99 @@ class ViewController: UIViewController {
         anzeigeEinheit.text = anzeigeWährendBerechnung
     }
 
-    @IBAction func aktionErgebnis() {
-        ergebnissAnzeige.text = "Ergebniss: "
+    @IBAction func aktionMal() {
+        anzeigeWährendBerechnung += "x"
+        anzeigeEinheit.text = anzeigeWährendBerechnung
     }
+    
+    @IBAction func aktionGeteilt() {
+        anzeigeWährendBerechnung += ":"
+        anzeigeEinheit.text = anzeigeWährendBerechnung
+    }
+    
+    func ArrayWertAusgabe<T>(vonWelchemArray ausgewähltesArray: [T], index: Int) ->T? {
+        var ergebnis: T?
+        for (key, value) in enumerate(ausgewähltesArray) {
+            if key == index {
+                ergebnis = value
+            }
+        }
+        return ergebnis
+    }
+    
+    func parser(#stringZuParsen: String, mitWelchemSeparator separator: String, berechne:Bool = false) ->[String] {
+        var stringZuParsenGeparst = stringZuParsen.componentsSeparatedByString(separator)
+        if berechne {
+            var temp = 0
+            var ergebnis: Int?
+            var wert = ArrayWertAusgabe(vonWelchemArray: stringZuParsenGeparst, index: 0)
+            //var wertInt = wert!.toInt()
+            for wertInt in stringZuParsenGeparst {
+                switch separator {
+                case "x":
+                    if ergebnis == nil {
+                        ergebnis = wert!.toInt()!
+                    } else {
+                        ergebnis! *= wertInt.toInt()!
+                    }
+                case ":":
+                    if ergebnis == nil {
+                        ergebnis = wert!.toInt()!
+                    } else {
+                        ergebnis! /= wertInt.toInt()!
+                    }
+                default: ergebnis! = 0
+                }
+            }
+            return ["\(ergebnis!)"]
+        } else {
+            return stringZuParsenGeparst
+        }
+    }
+    
+    
+    
+    @IBAction func aktionErgebnis() {
+        
+
+        var arrayOfWords = parser(stringZuParsen: anzeigeWährendBerechnung, mitWelchemSeparator: "+")
+        println("Ergebniss1: \n \(arrayOfWords)\n")
+        
+        
+        var arrayOfWords2 = [String]()
+        var temparray = [Int]()
+        for (index1, wort1) in enumerate(arrayOfWords) {
+            for c in wort1 {
+                if c == "x" {
+                    println ("String vor dem zweiten Parsen * \n \(wort1)\n")
+                    arrayOfWords2 = (parser(stringZuParsen: wort1, mitWelchemSeparator: "x", berechne: true))
+                    temparray.append(index1)
+                    for i in arrayOfWords2 {
+                        arrayOfWords.removeAtIndex(index1)
+                        arrayOfWords.insert(i, atIndex: index1)
+                    }
+                } else if c == ":" {
+                    println ("String vor dem zweiten Parsen : \n \(wort1)\n")
+                    arrayOfWords2 = (parser(stringZuParsen: wort1, mitWelchemSeparator: ":", berechne: true))
+                    temparray.append(index1)
+                    for i in arrayOfWords2 {
+                        arrayOfWords.removeAtIndex(index1)
+                        arrayOfWords.insert(i, atIndex: index1)
+                    }
+                }
+            }
+        }
+        println("Ergebniss2: \n \(arrayOfWords)\n")
+        
+        println("Das letzte Vorergebnis Lautet: \n \(arrayOfWords)\n")
+        var ergebnis = 0
+        for i in arrayOfWords {
+            ergebnis += i.toInt()!
+        }
+        println("Das Ergebnis Lautet: \n \(ergebnis)\n")
+        
+        ergebnissAnzeige.text = "Ergebnis: \(ergebnis)"
+    }
+    
 }
 
